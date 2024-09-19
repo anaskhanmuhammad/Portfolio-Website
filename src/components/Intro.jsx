47e5dda1405859641c79f2684684c1206599efff
Muhammad Anas Khan <muhammadanaskhan0212@gmail.com>
2024-09-20 01:21:28 +0500

    After Changes

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@ function Intro({id}) {
     <>
       <div className="intro" id={id}>
         <div className="Intro-text">
-            <h4>Hello</h4>
+            <h4>Hi</h4>
             <span className="name-span">My Name is <span className="main-name">Anas Khan</span></span>
             <p>Aspiring and enthusiastic web designer with a passion for creating visually appealing and user-friendly websites </p>
             <button className="Contact-button"><a href="#contact">Contact Me</a></button>
